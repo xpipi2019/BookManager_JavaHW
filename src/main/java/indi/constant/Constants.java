40513183d2149200c45e049de9c1aba08385f35a
xpipi2019 <1193820217@qq.com>
2025-01-04 17:14:56 +0800

    取消掉一些不应该出现在log里面的debug信息

--- src/main/java/indi/constant/Constants.java
+++ src/main/java/indi/constant/Constants.java
@@ -4,7 +4,7 @@ public interface Constants {
     // 定义配置文件名常量
     public static final String CONFIG_FILE = "config.ini";
     // 定义关于信息
-    public static final String aboutMessage = "Version: 0.2\n"
+    public static final String aboutMessage = "Version: 0.3\n"
             + "Author: xpipi,muelovo\n"
             + "Time: 2025/1/4\n";
 }
